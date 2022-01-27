5af997c58f1d73d5168413c3f96b30f054663bab
Liloctopus <93461415+Liloctopus@users.noreply.github.com>
2022-01-27 15:53:58 +0800

    Update index.js

--- Ilikemikuforever/EatKano-main/static/index.js
+++ Ilikemikuforever/EatKano-main/static/index.js
@@ -393,7 +393,7 @@
     w.initSetting = function() {
         document.getElementById("username").value = cookie("username") ? cookie("username") : "";
         document.getElementById("message").value = cookie("message") ? cookie("message") : "";
-        document.getElementsByTagName("title")[0].innerText = cookie("title") ? cookie("title") : "吃掉小鹿乃";
+        document.getElementsByTagName("title")[0].innerText = cookie("title") ? cookie("title") : "Miku Hatsune";
         if (cookie("keyboard")) {
             document.getElementById("keyboard").value = cookie("keyboard");
             map = {}
